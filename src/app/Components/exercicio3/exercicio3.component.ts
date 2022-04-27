09c30b5fc135d59920bf03ffd930985bdb21b1f8
matheusgevartosky <magevartosky@gmail.com>
2022-04-27 15:52:25 -0300

    Lista-Semana-8-Angular-DevInHouse

---
+++ src/app/Components/exercicio3/exercicio3.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio3',
+  templateUrl: './exercicio3.component.html',
+  styleUrls: ['./exercicio3.component.scss']
+})
+export class Exercicio3Component {
+  value1!:number ;
+  value2!:string;
+  operator!:string;
+  result!:number
+
+  constructor() { }
+
+
+
+}
